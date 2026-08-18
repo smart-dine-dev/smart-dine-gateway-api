0d4636f6103acb62d97ecba0fee1790be47d4add
education-bootcamp <education.seekerscloud@gmail.com>
2026-08-18 21:28:38 +0530

    init

---
+++ src/main/java/com/devstack/SmartDine/config/GatewayConfig.java
@@ -0,0 +1,14 @@
+package com.devstack.SmartDine.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes().build();
+    }
+}
